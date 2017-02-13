2eec5cc45fc0ddf3815bc2d7d963fec3aa49445b
kennanseno <k_seno@yahoo.com>
2017-02-13 15:11:52 +0000

    part 2 WIP

--- Lab2/index.js
+++ Lab2/index.js
@@ -8,61 +8,27 @@ app.use(bodyParser());
 app.use(express.static('public'));
 
 //connect to postgres database
-var db = massive.connectSync({db: 'postgres'});
+var db = massive.connectSync({
+	db: 'pgguide'
+});
 
 app.listen(3000, function() {
     console.log('Application listening on Port 3000');
 });
 
-app.get('/products', function(req, res) {
-	db.products.find({}, function(err, result) {
-		res.send(result);
-	})
-});
-
-app.get('/users', function(req, res) {
-    db.users.find({}, function(err, result) {
-		res.send(result);
-	})
-});
-
-app.get('/purchases', function(req, res) {
-    db.purchases.find({}, function(err, result) {
-		res.send(result);
-	})
-});
-
-app.get('/products/:id', function(req, res) {
-	var id = req.params.id;
+app.get('/products/hack/', function(req, res) {
+	var id = req.query.id;
 	
-	console.log("Before:", id);
-	db.run("select * from products where id = " + id, function(err, docs){
-		console.log("After:", docs);
+	db.run('select * from products where id = ' + id, function(err, docs){
 		res.send(docs);
 	});
 });
 
-app.get('/users', function (req, res) {
+// PARAMETERIZED FIX
+app.get('/products/1', function(req, res) {
+	var id = req.query.id;
 	
-
-})
-
-app.get('/users/:id', function(req, res) {
-	var data = {
-		id: req.params.id
-	}
-	
-    db.users.find(data, function(err, result) {
-		res.send(result);
-	})
-});
-
-app.get('/purchases/:id', function(req, res) {
-	var data = {
-		id: req.params.id
-	}
-
-    db.purchases.find(data, function(err, result) {
-		res.send(result);
-	})
+	db.run('select * from products where id = $1', [id], function(err, docs){
+		res.send(docs);
+	});
 });
